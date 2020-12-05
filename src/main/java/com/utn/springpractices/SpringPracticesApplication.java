f925a291a44fdf3d63aff3cbd5d63d6505a8752a
JLafratta <julian.lafratta@gmail.com>
2020-12-05 18:12:28 -0300

    initial files

---
+++ src/main/java/com/utn/springpractices/SpringPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.utn.springpractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/springpractices/SpringPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.springpractices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
