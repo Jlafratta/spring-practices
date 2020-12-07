ebc925d56bc00a37e68a14ab43ef895b30f379e4
JLafratta <julian.lafratta@gmail.com>
2020-12-07 03:56:05 -0300

    base path /api added

--- src/main/java/com/utn/springpractices/config/SwaggerConfig.java
+++ src/main/java/com/utn/springpractices/config/SwaggerConfig.java
@@ -1,13 +1,14 @@
 package com.utn.springpractices.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@org.springframework.context.annotation.Configuration
+@Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
