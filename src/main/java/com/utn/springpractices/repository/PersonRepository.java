01c53478d2f518e2f4529f7fe3945943b9a59af4
Jlafratta <julian.lafratta@gmail.com>
2020-12-09 03:24:55 -0300

    native query

--- src/main/java/com/utn/springpractices/repository/PersonRepository.java
+++ src/main/java/com/utn/springpractices/repository/PersonRepository.java
@@ -2,6 +2,7 @@ package com.utn.springpractices.repository;
 
 import com.utn.springpractices.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Integer> {
 
+    @Query(value = "SELECT * FROM person WHERE firstname = ?1", nativeQuery = true)
     List<Person> findByFirstname(String firstname);
 
     Integer deleteByFirstnameAndLastname(String firstname, String lastname);
